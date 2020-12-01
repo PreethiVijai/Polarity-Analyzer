b87f0f111e4a113d51cf03ff1cc5efd3553a140e
PreethiVijai <prvi9376@colorado.edu>
2020-12-01 16:30:56 -0700

    Initial setup Rabbit MQ

---
+++ application/Analyzer/src/RabbitReceiver.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+import pika
+import sys
+
+
+class RabbitReceiver:
+    """Receives messages from RabbitMQ"""
+
+    def __init__(self, queue_name: str, receiver_callback: Callable) -> None:
+        """
+        Initialize RabbitMQ connection
+
+        queue_name: str - Name of queue to receive from
+        receiver_callback: Callable - Function to process received messages
+        """
+        self.queue_name = queue_name
+        self.receiver_callback = receiver_callback
+
+    def prepare_connection(self, rabbit_host: str):
+        """
+        Prepare RabbitMQ connection
+
+        rabbit_host: str - Hostname of RabbitMQ to connect to
+        """
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
+
+    def callback(self, ch, method, properties, body) -> None:
+        try:
+            self.receiver_callback(body)
+        except:
+            print(sys.exc_info())
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def start_receiving(self) -> None:
+        """Begin receiving messages"""
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.callback)
+        self.channel.start_consuming()
+
+    def stop_receiving(self) -> None:
+        self.channel.close()
---
+++ application/Collector/src/Collector.py
@@ -0,0 +1,49 @@
+import argparse
+from time import sleep
+import requests
+
+from Twitter_API import API
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('apikey', help='API key for the app communicating with Twitter')
+    parser.add_argument('apisecret', help='API secret for the app communicating with Twitter')
+    parser.add_argument('rabbit_host', help='Hostname of RabbitMQ instance to connect to')
+    return parser.parse_args()
+
+
+def main(apikey, apisecret, rabbit_host):
+    #apikey = get_file_conents(apikey)
+    #apisecret = get_file_conents(apisecret)
+
+    # Prepare Twitter and RabbitMQ connections
+    api = API(apikey, apisecret, rabbit_host)
+    api.get_oauth2_bearer_token()
+
+    # Example of adding a rule:
+    # api.add_filter_rule('fire OR emergency OR shooting')
+
+    # Example of printing out existing rules
+    # print('Filter rules: ', api.get_filter_rules()['data'])
+
+    # Example of deleting existing rules
+    # api.delete_all_filter_rules()
+
+    # Example of opening filtered tweet stream, using exponential
+    # backoff if reconnection is needed to avoid rate limiting
+    twitter_timeout = 0
+    while True:
+        api.start_filtered_tweet_stream()
+        print('Warning: Twitter stream disconnected, reconnecting after', 2**twitter_timeout, 'seconds')
+        sleep(2 ** twitter_timeout)
+        # Exponential backoff up to around 8.5 minutes (2**9 seconds)
+        if (twitter_timeout < 9):
+            twitter_timeout += 1
+
+
+if __name__ == '__main__':
+    print("launching\n")
+    args = parse_args()
+    main(args.apikey, args.apisecret, args.rabbit_host)
+
